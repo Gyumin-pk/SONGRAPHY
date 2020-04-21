b83ded8cf541c0a836d558ea1e7b4fc5cbd8c498
Gyumin Park <gyuminpk@Gyuminui-MacBookPro.local>
2020-04-21 17:09:09 +0900

    Updates

---
+++ precache-manifest.3c9fdc562eef32a109e396d43ec8951c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d829d62b65d9ab042e245d228fe10fbc",
+    "url": "/SONGRAPHY/index.html"
+  },
+  {
+    "revision": "d08b2cfffd0d0f81691e",
+    "url": "/SONGRAPHY/static/css/main.be7153dc.chunk.css"
+  },
+  {
+    "revision": "380dfed9de04ff1c7eb7",
+    "url": "/SONGRAPHY/static/js/2.8b798851.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SONGRAPHY/static/js/2.8b798851.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d08b2cfffd0d0f81691e",
+    "url": "/SONGRAPHY/static/js/main.ff09a713.chunk.js"
+  },
+  {
+    "revision": "557ae93afefcc11bb914",
+    "url": "/SONGRAPHY/static/js/runtime-main.0bc65184.js"
+  }
+]);
